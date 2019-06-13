52f0acbf64adfa6fe3b99cad3d87d188660c73fb
Marc-André Lureau <marcandre.lureau@redhat.com>
2019-06-13 15:38:37 +0200

    qemu: reset VM id after external devices stop
    pid filenames are based on VM shortname, which is derived from VM id.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -7269,8 +7269,6 @@ void qemuProcessStop(virQEMUDriverPtr driver,
 
     qemuProcessBuildDestroyMemoryPaths(driver, vm, NULL, false);
 
-    vm->def->id = -1;
-
     if (virAtomicIntDecAndTest(&driver->nactive) && driver->inhibitCallback)
         driver->inhibitCallback(false, driver->inhibitOpaque);
 
@@ -7345,6 +7343,8 @@ void qemuProcessStop(virQEMUDriverPtr driver,
     qemuDomainCleanupRun(driver, vm);
 
     qemuExtDevicesStop(driver, vm);
+
+    vm->def->id = -1;
 
     /* Stop autodestroy in case guest is restarted */
     qemuProcessAutoDestroyRemove(driver, vm);
